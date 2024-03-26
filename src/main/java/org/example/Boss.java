bfa5a01e0f196c7854f41db88ae4a51e5e000dcb
beka111 <abakirovbektur0@gmail.com>
2024-03-26 18:52:24 +0600

    Initial commit

---
+++ src/main/java/org/example/Boss.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.example.Weapon;
+
+public class Boss extends GameEntity {
+    Weapon weapon = new Weapon();
+
+    public Boss(int health, int damage) {
+        super(health, damage);
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public String info() {
+        return "Boss health: " + getHealth()
+                + "\nBoss damage: " + getDamage()
+                + "\nBoss weapon type: " + weapon.getTypeOfWeapon()
+                + "\nBoss weapon: " + weapon.getNameOfWeapon();
+    }
+}
---
+++ src/main/java/org/example/GameEntity.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class GameEntity {
+    private int health;
+    private int damage;
+    private String name;
+
+    public GameEntity(int health, int damage) {
+        this.damage = damage;
+        this.health = health;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int healh) {
+        this.health = healh;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
